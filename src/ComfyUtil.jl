c0ce082476c3c2c4e805f2612154a2fdb7c0603d
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2017-11-09 15:51:40 +0100

    Minor change to Lincense-related src doc

--- src/ComfyUtil.jl
+++ src/ComfyUtil.jl
@@ -17,7 +17,7 @@
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with ComfyBase.  If not, see <http://www.gnu.org/licenses/>.
+# along with ComfyUtil.  If not, see <http://www.gnu.org/licenses/>.
 # 
 
 module ComfyUtil
